d60c81615b2301985decdb2e0e45cb4a8b705949
wockd3325 <wockd3325@gmail.com>
2024-01-03 22:56:59 +0900

    Initial commit

---
+++ src/main/java/kr/co/spring_data_jpa_study/study/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.spring_data_jpa_study.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/spring_data_jpa_study/study/SpringDataJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.spring_data_jpa_study.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
